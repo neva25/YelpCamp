d06f87271a7c7b791042168c43087e93bb0be075
NevaBurges <nevaburges@hotmail.com>
2021-07-28 00:44:13 +0530

    campground create and show pages

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -29,6 +30,22 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find();
   res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", async (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
+});
+
+app.post("/campgrounds", async (req, res) => {
+  console.log(req.body);
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
