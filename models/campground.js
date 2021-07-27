55c48e49df57bd49b3dffd8989a7665878e8a009
NevaBurges <nevaburges@hotmail.com>
2021-07-27 23:22:25 +0530

    campground index page

--- models/campground.js
+++ models/campground.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-  title: string,
-  price: string,
-  description: string,
-  location: string,
+  title: String,
+  price: String,
+  description: String,
+  location: String,
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
