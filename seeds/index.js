2b8451c07450a4d0c512630e580933e9815c289d
NevaBurges <nevaburges@hotmail.com>
2021-07-29 20:19:32 +0530

    campground index page styling

--- seeds/index.js
+++ seeds/index.js
@@ -1,36 +1,40 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/429524/",
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem voluptate debitis minus iusto provident quibusdam labore tempore. Temporibus pariatur voluptatibus aliquam, fugit atque voluptas, possimus architecto necessitatibus tempore recusandae quae!",
+      price,
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
