dc1edb2f4b05ef8a0a7d60890f07c444c6a896f4
NevaBurges <nevaburges@hotmail.com>
2021-08-02 14:10:55 +0530

    show campground author

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -31,9 +31,9 @@ router.get("/new", isLoggedIn, (req, res) => {
 router.get(
   "/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
+    const campground = await Campground.findById(req.params.id)
+      .populate("reviews")
+      .populate("author");
     if (!campground) {
       req.flash("error", "Cannot find this campground!");
       return res.redirect("/campgrounds");
