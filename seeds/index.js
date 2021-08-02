f092629642bd24916baffa71db1b5686908693d1
NevaBurges <nevaburges@hotmail.com>
2021-08-02 14:08:00 +0530

    author added to campground schema

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
+const User = require("../models/user");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -20,10 +21,15 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
+  await User.deleteMany({});
+  const user = new User({ email: "neva@hotmail.com", username: "Neva" });
+  await User.register(user, "password");
+
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: user,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/429524/",
