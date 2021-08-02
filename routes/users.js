fd9c50be87d1d7986ba384fb96d5dd81351dc7b4
NevaBurges <nevaburges@hotmail.com>
2021-08-02 12:38:34 +0530

    return to url on login

--- routes/users.js
+++ routes/users.js
@@ -11,9 +11,12 @@ router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body.user;
     const user = new User({ username, email });
-    await User.register(user, password);
-    req.flash("success", "Welcome to YelpCamp!");
-    res.redirect("/campgrounds");
+    const registeredUser = await User.register(user, password);
+    req.login(registeredUser, (err) => {
+      if (err) return next(err);
+      req.flash("success", "Welcome to YelpCamp!");
+      res.redirect("/campgrounds");
+    });
   } catch (e) {
     req.flash("error", e.message);
     res.redirect("/register");
@@ -32,11 +35,13 @@ router.post(
   }),
   (req, res) => {
     req.flash("success", "Welcome back!");
-    res.redirect("/campgrounds");
+    const redirectUrl = req.session.returnTo || "/campgrounds";
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
   }
 );
 
-router.post("/logout", (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout();
   req.flash("success", "Goodbye!");
   res.redirect("/campgrounds");
