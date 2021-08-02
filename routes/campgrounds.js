278a9c109901546c4444597b17153f5fc8329127
NevaBurges <nevaburges@hotmail.com>
2021-08-02 14:13:17 +0530

    campground author

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -48,6 +48,7 @@ router.post(
   validateCampground,
   catchAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash("success", "Successfully made a new campground!");
     res.redirect(`/campgrounds/${campground._id}`);
