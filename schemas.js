29636939722171fbc2d5c31a72680dd1136cf9fe
NevaBurges <nevaburges@hotmail.com>
2021-08-03 14:37:55 +0530

    schema changes

--- schemas.js
+++ schemas.js
@@ -4,7 +4,14 @@ module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
     price: Joi.number().required().min(0),
-    image: Joi.string().required(),
+    // images: Joi.array()
+    //   .items(
+    //     Joi.object({
+    //       url: Joi.string().required(),
+    //       filename: Joi.string().required(),
+    //     }).required()
+    //   )
+    //   .required(),
     location: Joi.string().required(),
     description: Joi.string().required(),
   }).required(),
