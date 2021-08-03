667d67bce805b088ad37fc6ec2fa7fdd51ec84b7
NevaBurges <nevaburges@hotmail.com>
2021-08-03 09:11:25 +0530

    fix

--- middleware.js
+++ middleware.js
@@ -25,7 +25,7 @@ module.exports.validateCampground = (req, res, next) => {
 module.exports.isOwner = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
-  if (!campground.author.equals(req.author._id)) {
+  if (!campground.author.equals(req.user._id)) {
     req.flash("error", "You do not have permission to do that!");
     return res.redirect(`/campgrounds/${id}`);
   }
