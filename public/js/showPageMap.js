6de63b8901e16764ab7580a58f840e45c6302384
NevaBurges <nevaburges@hotmail.com>
2021-08-04 16:50:19 +0530

    map with geocoded location

---
+++ public/js/showPageMap.js
@@ -0,0 +1,14 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: "map",
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: campground.geometry.coordinates,
+    zoom: 10
+})
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+    )
+    .addTo(map)
