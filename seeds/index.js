4fedb5d40876a6d61a96ccc34dcc5832b5ef8ebf
NevaBurges <nevaburges@hotmail.com>
2021-08-05 00:39:49 +0530

    cluster maps

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const seedDB = async () => {
   const user = new User({ email: "neva@hotmail.com", username: "Neva" });
   await User.register(user, "password");
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
