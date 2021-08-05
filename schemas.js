36a6f5ad8ee64b089c493272e585ef0f99c8bf02
NevaBurges <nevaburges@hotmail.com>
2021-08-05 20:03:41 +0530

    sanitize html

--- schemas.js
+++ schemas.js
@@ -1,17 +1,33 @@
 const Joi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+  type: "string",
+  base: joi.string(),
+  messages: {
+    "string.escapeHTML": "{{#label}} must not include HTML!",
+  },
+  rules: {
+    escapeHTML: {
+      validate(value, helpers) {
+        const clean = sanitizeHtml(value, {
+          allowedTags: [],
+          allowedAttributes: {},
+        });
+        if (clean !== value)
+          return helpers.error("string.escapeHTML", { value });
+        return clean;
+      },
+    },
+  },
+});
+
+const Joi = Joi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
     title: Joi.string().required(),
     price: Joi.number().required().min(0),
-    // images: Joi.array()
-    //   .items(
-    //     Joi.object({
-    //       url: Joi.string().required(),
-    //       filename: Joi.string().required(),
-    //     }).required()
-    //   )
-    //   .required(),
     location: Joi.string().required(),
     description: Joi.string().required(),
   }).required(),
